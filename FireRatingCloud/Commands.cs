91b81f961b15b3181fcf983a63eb6d6d8f125441
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2015-07-05 18:09:39 +0200

    removed old code to generate excel spreadsheet

--- FireRatingCloud/Commands.cs
+++ FireRatingCloud/Commands.cs
@@ -7,17 +7,16 @@ using Autodesk.Revit.ApplicationServices;
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
-using Autodesk.Revit.UI.Selection;
 #endregion
 
 namespace FireRatingCloud
 {
-  #region Lab4_3_1_CreateAndBindSharedParam
+  #region Cmd_1_CreateAndBindSharedParameter
   /// <summary>
   /// Create and bind shared parameter.
   /// </summary>
   [Transaction( TransactionMode.Manual )]
-  public class Lab4_3_1_CreateAndBindSharedParam : IExternalCommand
+  public class Cmd_1_CreateAndBindSharedParameter : IExternalCommand
   {
     // What element type are we interested in? the standard SDK FireRating
     // sample uses BuiltInCategory.OST_Doors. we also test using
@@ -48,6 +47,8 @@ namespace FireRatingCloud
       Document doc = uiapp.ActiveUIDocument.Document;
       Category cat = null;
 
+      // The category to define the parameter for.
+
       #region Determine model group category
 #if DETERMINE_MODEL_GROUP_CATEGORY
       List<Element> modelGroups = new List<Element>();
@@ -64,119 +65,105 @@ namespace FireRatingCloud
 
       if( null == cat )
       {
-        // The category we are defining the parameter for
-
-        try
-        {
-          cat = doc.Settings.Categories.get_Item( Target );
-        }
-        catch( Exception ex )
-        {
-          message = "Error obtaining the shared param document category: "
-            + ex.Message;
-          return Result.Failed;
-        }
-        if( null == cat )
-        {
-          message = "Unable to obtain the shared param document category.";
-          return Result.Failed;
-        }
+        cat = doc.Settings.Categories.get_Item( Target );
       }
 
-      // Get the current shared params definition file
+      // Retrieve shared parameter definition file.
 
-      DefinitionFile sharedParamsFile = LabUtils.GetSharedParamsFile( app );
+      DefinitionFile sharedParamsFile = Util
+        .GetSharedParamsFile( app );
+
       if( null == sharedParamsFile )
       {
         message = "Error getting the shared params file.";
         return Result.Failed;
       }
 
-      // Get or create the shared params group
+      // Get or create the shared parameter group.
 
-      DefinitionGroup sharedParamsGroup = LabUtils.GetOrCreateSharedParamsGroup(
-        sharedParamsFile, LabConstants.SharedParamsGroupAPI );
+      DefinitionGroup sharedParamsGroup
+        = sharedParamsFile.Groups.get_Item(
+          Util.SharedParameterGroupName );
+
       if( null == sharedParamsGroup )
       {
-        message = "Error getting the shared params group.";
-        return Result.Failed;
+        sharedParamsGroup = sharedParamsFile.Groups
+          .Create( Util.SharedParameterGroupName );
       }
 
-      // Visibility of the new parameter: the 
-      // Category.AllowsBoundParameters property 
+      // Visibility of the new parameter: the
+      // Category.AllowsBoundParameters property
       // determines whether a category is allowed to
-      // have user-visible shared or project parameters. 
+      // have user-visible shared or project parameters.
       // If it is false, it may not be bound to visible
-      // shared parameters using the BindingMap. Note 
-      // that non-user-visible parameters can still be 
+      // shared parameters using the BindingMap. Note
+      // that non-user-visible parameters can still be
       // bound to these categories.
 
       bool visible = cat.AllowsBoundParameters;
 
       // Get or create the shared params definition
 
-      Definition fireRatingParamDef = LabUtils.GetOrCreateSharedParamsDefinition(
-        sharedParamsGroup, ParameterType.Number, LabConstants.SharedParamsDefFireRating, visible );
-      if( null == fireRatingParamDef )
+      Definition def = sharedParamsGroup.Definitions
+        .get_Item( Util.SharedParameterName );
+
+      if( null == def )
       {
-        message = "Error in creating shared parameter.";
-        return Result.Failed;
+        ExternalDefinitionCreationOptions opt
+          = new ExternalDefinitionCreationOptions(
+            Util.SharedParameterName,
+            ParameterType.Number );
+
+        opt.Visible = visible;
+
+        def = sharedParamsGroup.Definitions.Create(
+          opt );
       }
 
-      // Create the category set for binding and add the category
-      // we are interested in, doors or walls or whatever:
+      //if( null == def )
+      //{
+      //  message = "Error creating shared parameter.";
+      //  return Result.Failed;
+      //}
+
+      // Create the category set for binding and
+      // add the category of interest to it.
 
       CategorySet catSet = app.Create.NewCategorySet();
-      try
-      {
-        catSet.Insert( cat );
-      }
-      catch( Exception )
-      {
-        message = string.Format(
-          "Error adding '{0}' category to parameters binding set.",
-          cat.Name );
-        return Result.Failed;
-      }
+
+      catSet.Insert( cat );
 
       // Bind the parameter.
 
-      try
+      using( Transaction t = new Transaction( doc ) )
       {
-        using( Transaction t = new Transaction( doc ) )
-        {
-          t.Start( "Bind Fire Rating Shared Parameter" );
+        t.Start( "Bind Fire Rating Shared Parameter" );
 
-          Binding binding = app.Create.NewInstanceBinding( catSet );
+        Binding binding = app.Create.NewInstanceBinding( catSet );
 
-          // We could check if already bound, but looks 
-          // like Insert will just ignore it in that case.
+        // We could check if already bound, but looks
+        // like Insert will just ignore it in that case.
 
-          doc.ParameterBindings.Insert( fireRatingParamDef, binding );
+        doc.ParameterBindings.Insert( def, binding );
 
-          // You can also specify the parameter group here:
+        // You can also specify the parameter group here:
 
-          //doc.ParameterBindings.Insert( fireRatingParamDef, binding, BuiltInParameterGroup.PG_GEOMETRY );
+        //doc.ParameterBindings.Insert( fireRatingParamDef, binding, BuiltInParameterGroup.PG_GEOMETRY );
 
-          t.Commit();
-        }
-      }
-      catch( Exception ex )
-      {
-        message = ex.Message;
-        return Result.Failed;
+        t.Commit();
       }
       return Result.Succeeded;
     }
   }
-  #endregion // Lab4_3_1_CreateAndBindSharedParam
+  #endregion // Cmd_1_CreateAndBindSharedParameter
 
-  #region Lab4_3_2_ExportSharedParamToExcel
+  #region Cmd_2_ExportSharedParameterValues
   /// <summary>
-  /// Export all target element ids and their FireRating param values to Excel.
+  /// Export all target element ids and their
+  /// FireRating parameter values to external database.
   /// </summary>
   [Transaction( TransactionMode.ReadOnly )]
-  public class Lab4_3_2_ExportSharedParamToExcel : IExternalCommand
+  public class Cmd_2_ExportSharedParameterValues : IExternalCommand
   {
     public Result Execute(
       ExternalCommandData commandData,
@@ -188,76 +175,275 @@ namespace FireRatingCloud
       Document doc = uiapp.ActiveUIDocument.Document;
 
       Category cat = doc.Settings.Categories.get_Item(
-        Lab4_3_1_CreateAndBindSharedParam.Target );
+        Cmd_1_CreateAndBindSharedParameter.Target );
 
-      // Launch Excel (same as in Lab 4_2, so we really 
+      #region OLD_CODE
+      // Launch Excel (same as in Lab 4_2, so we really
       // should have better created some utils...)
 
-      X.Application excel = new X.ApplicationClass();
-      if( null == excel )
-      {
-        LabUtils.ErrorMsg( "Failed to get or start Excel." );
-        return Result.Failed;
-      }
-      excel.Visible = true;
-      X.Workbook workbook = excel.Workbooks.Add( Missing.Value );
-      X.Worksheet worksheet;
-      //while( 1 < workbook.Sheets.Count )
+      //X.Application excel = new X.ApplicationClass();
+      //if( null == excel )
       //{
-      //  worksheet = workbook.Sheets.get_Item( 0 ) as X.Worksheet;
-      //  worksheet.Delete();
+      //  Util.ErrorMsg( "Failed to get or start Excel." );
+      //  return Result.Failed;
       //}
-      worksheet = excel.ActiveSheet as X.Worksheet;
-      worksheet.Name = "Revit " + cat.Name;
-      worksheet.Cells[1, 1] = "ID";
-      worksheet.Cells[1, 2] = "Level";
-      worksheet.Cells[1, 3] = "Tag";
-      worksheet.Cells[1, 4] = LabConstants.SharedParamsDefFireRating;
-      worksheet.get_Range( "A1", "Z1" ).Font.Bold = true;
+      //excel.Visible = true;
+      //X.Workbook workbook = excel.Workbooks.Add( Missing.Value );
+      //X.Worksheet worksheet;
+      ////while( 1 < workbook.Sheets.Count )
+      ////{
+      ////  worksheet = workbook.Sheets.get_Item( 0 ) as X.Worksheet;
+      ////  worksheet.Delete();
+      ////}
+      //worksheet = excel.ActiveSheet as X.Worksheet;
+      //worksheet.Name = "Revit " + cat.Name;
+      //worksheet.Cells[1, 1] = "ID";
+      //worksheet.Cells[1, 2] = "Level";
+      //worksheet.Cells[1, 3] = "Tag";
+      //worksheet.Cells[1, 4] = Util.SharedParamsDefFireRating;
+      //worksheet.get_Range( "A1", "Z1" ).Font.Bold = true;
+      #endregion // OLD_CODE
 
-      List<Element> elems = LabUtils.GetTargetInstances( doc,
-        Lab4_3_1_CreateAndBindSharedParam.Target );
+      FilteredElementCollector collector
+        = Util.GetTargetInstances( doc,
+          Cmd_1_CreateAndBindSharedParameter.Target );
 
-      // Get Shared param Guid
+      // Get shared parameter GUID.
 
-      Guid paramGuid = LabUtils.SharedParamGUID( app,
-        LabConstants.SharedParamsGroupAPI, LabConstants.SharedParamsDefFireRating );
+      Guid paramGuid = Util.SharedParamGuid( app,
+        Util.SharedParameterGroupName,
+        Util.SharedParameterName );
 
       if( paramGuid.Equals( Guid.Empty ) )
       {
-        LabUtils.ErrorMsg( "No Shared param found in the file - aborting..." );
+        message = "Shared parameter GUID not found.";
         return Result.Failed;
       }
 
-      // Loop through all elements and export each to an Excel row
+      // Loop through all selected elements and export
+      // parameter value for each.
 
-      int row = 2;
-      foreach( Element e in elems )
+      int n = collector.Count<Element>();
+
+      string[] records = new string[n];
+
+      //int row = 2;
+
+      int i = 0;
+
+      foreach( Element e in collector )
       {
-        worksheet.Cells[row, 1] = e.Id.IntegerValue; // ID
+        #region OLD_CODE
+        //worksheet.Cells[row, 1] = e.Id.IntegerValue; // ID
 
-        //worksheet.Cells[row, 2] = e.Level.Name; // Level // 2013
-        worksheet.Cells[row, 2] = doc.GetElement( e.LevelId ).Name; // Level // 2014
+        ////worksheet.Cells[row, 2] = e.Level.Name; // Level // 2013
+        //worksheet.Cells[row, 2] = doc.GetElement( e.LevelId ).Name; // Level // 2014
 
-        // Tag:
+        //// Tag:
 
-        Parameter tagParameter = e.get_Parameter( BuiltInParameter.ALL_MODEL_MARK );
-        if( null != tagParameter )
+        //Parameter tagParameter = e.get_Parameter( BuiltInParameter.ALL_MODEL_MARK );
+        //if( null != tagParameter )
+        //{
+        //  worksheet.Cells[row, 3] = tagParameter.AsString();
+        //}
+
+        //// FireRating:
+
+        //Parameter parameter = e.get_Parameter( paramGuid );
+        //if( null != parameter )
+        //{
+        //  worksheet.Cells[row, 4] = parameter.AsDouble();
+        //}
+        //++row;
+        #endregion // OLD_CODE
+
+        records[i++] = string.Format( "[{0},{1},{2},{3}]",
+          e.UniqueId,
+          doc.GetElement( e.LevelId ).Name,
+          e.get_Parameter( BuiltInParameter.ALL_MODEL_MARK ).AsString(),
+          e.get_Parameter( paramGuid ).AsDouble() );
+      }
+
+      string json = "[" + string.Join( ",", records ) + "]";
+
+      Debug.Print( json );
+
+      return Result.Succeeded;
+    }
+  }
+  #endregion // Cmd_2_ExportSharedParameterValues
+
+  #region Cmd_3_ImportSharedParameterValues
+  /// <summary>
+  /// Import updated FireRating param values from Excel.
+  /// </summary>
+  [Transaction( TransactionMode.Manual )]
+  public class Cmd_3_ImportSharedParameterValues : IExternalCommand
+  {
+    /// <summary>
+    /// Extract a list of strings representing the
+    /// elements from the given JSON-formatted list.
+    /// Return true on success, false otherwise.
+    /// </summary>
+    bool GetJsonListElements(
+      string json,
+      out string[] elements )
+    {
+      json.Trim();
+
+      int n = json.Length;
+
+      elements = ( '[' == json[0] && ']' == json[n - 1] )
+        ? json.Substring( 1, n - 2 ).Split( ',' )
+        : null;
+
+      return null != elements;
+    }
+
+    public Result Execute(
+      ExternalCommandData commandData,
+      ref string message,
+      ElementSet elements )
+    {
+      UIApplication uiapp = commandData.Application;
+      Application app = uiapp.Application;
+      Document doc = uiapp.ActiveUIDocument.Document;
+
+      Guid paramGuid = Util.SharedParamGuid( app,
+        Util.SharedParameterGroupName,
+        Util.SharedParameterName );
+
+      if( paramGuid.Equals( Guid.Empty ) )
+      {
+        message = "Shared parameter GUID not found.";
+        return Result.Failed;
+      }
+
+      #region OLD_CODE
+      // Let user select the Excel file.
+
+      //WinForms.OpenFileDialog dlg = new WinForms.OpenFileDialog();
+      //dlg.Title = "Select source Excel file from which to update Revit shared parameters";
+      //dlg.Filter = "Excel spreadsheet files (*.xls;*.xlsx)|*.xls;*.xlsx|All files (*)|*";
+      //if( WinForms.DialogResult.OK != dlg.ShowDialog() )
+      //{
+      //  return Result.Cancelled;
+      //}
+
+      // Launch/Get Excel via COM Interop:
+
+      //X.Application excel = new X.Application();
+      //if( null == excel )
+      //{
+      //  Util.ErrorMsg( "Failed to get or start Excel." );
+      //  return Result.Failed;
+      //}
+      //excel.Visible = true;
+      //X.Workbook workbook = excel.Workbooks.Open( dlg.FileName,
+      //  Missing.Value, Missing.Value, Missing.Value,
+      //  Missing.Value, Missing.Value, Missing.Value, Missing.Value,
+      //  Missing.Value, Missing.Value, Missing.Value, Missing.Value,
+      //  Missing.Value, Missing.Value, Missing.Value );
+      //X.Worksheet worksheet = workbook.ActiveSheet as X.Worksheet;
+      #endregion // OLD_CODE
+
+      string json = ""; // [[id,level,tag,firerating],...]
+
+      string[] records;
+
+      if( !GetJsonListElements( json, out records ) )
+      {
+        message = "Error parsing JSON input.";
+        return Result.Failed;
+      }
+
+      using( Transaction t = new Transaction( doc ) )
+      {
+        t.Start( "Import Fire Rating Values" );
+
+        // Retrieve element unique id and FireRating parameter values.
+
+        string[] values;
+
+        foreach( string record in records )
         {
-          worksheet.Cells[row, 3] = tagParameter.AsString();
+          if( !GetJsonListElements( record, out values ) )
+          {
+            message = "Error parsing JSON input.";
+            return Result.Failed;
+          }
+          Element e = doc.GetElement( values[0] );
+
+          if( null == e )
+          {
+            message = string.Format(
+              "Error retrieving element for unique id {0}.",
+              values[0] );
+            return Result.Failed;
+          }
+
+          Parameter p = e.get_Parameter( paramGuid );
+
+          if( null == p )
+          {
+            message = string.Format(
+              "Error retrieving shared parameter on element with unique id {0}.",
+              values[0] );
+            return Result.Failed;
+          }
+
+          p.Set( double.Parse( values[3] ) );
         }
 
-        // FireRating:
+        #region OLD_CODE
+        //int id;
+        //double fireRatingValue;
+        //int row = 2;
+        //while( true )
+        //{
+        //  try
+        //  {
+        //    // Extract relevant XLS values.
 
-        Parameter parameter = e.get_Parameter( paramGuid );
-        if( null != parameter )
-        {
-          worksheet.Cells[row, 4] = parameter.AsDouble();
-        }
-        ++row;
+        //    X.Range r = worksheet.Cells[row, 1] as X.Range;
+        //    if( null == r.Value2 )
+        //    {
+        //      break;
+        //    }
+        //    double d = (double) r.Value2;
+        //    id = (int) d;
+        //    if( 0 >= id )
+        //    {
+        //      break;
+        //    }
+        //    r = worksheet.Cells[row, 4] as X.Range;
+        //    fireRatingValue = (double) r.Value2;
+
+        //    // Get document's door element via Id
+
+        //    ElementId elementId = new ElementId( id );
+        //    Element door = doc.GetElement( elementId );
+
+        //    // Set the param
+
+        //    if( null != door )
+        //    {
+        //      //Parameter parameter = door.get_Parameter( Util.SharedParamsDefFireRating );
+        //      Parameter parameter = door.get_Parameter( paramGuid );
+        //      parameter.Set( fireRatingValue );
+        //    }
+        //  }
+        //  catch( Exception )
+        //  {
+        //    break;
+        //  }
+        //  ++row;
+        #endregion // OLD_CODE
+
+        t.Commit();
       }
       return Result.Succeeded;
     }
   }
-  #endregion // Lab4_3_2_ExportSharedParamToExcel
+  #endregion // Cmd_3_ImportSharedParameterValues
 }
