a592f82e66dca6c9e37f0d8f94c59000e732233c
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2015-07-08 17:43:27 +0200

    moved http access code out of Cmd_2_ExportSharedParameterValues into Util class

--- FireRatingCloud/Commands.cs
+++ FireRatingCloud/Commands.cs
@@ -1,25 +1,26 @@
 #region Namespaces
 using System;
-using System.Collections.Generic;
+//using System.Collections.Generic;
+//using System.IO;
+using System.Collections;
 using System.Diagnostics;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Text;
+//using System.Net;
+//using System.Net.Http;
+//using System.Net.Http.Headers;
+//using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+//using System.Threading.Tasks;
+//using System.Web.Script.Serialization;
 using Autodesk.Revit.ApplicationServices;
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
-using System.IO;
-using System.Web.Script.Serialization;
-using System.Collections;
 #endregion
 
 namespace FireRatingCloud
 {
+  #region Project data helper class for JavaScriptSerializer
 #if USE_JavaScriptSerializer
   /// <summary>
   /// Data holder to use JavaScriptSerializer.
@@ -35,6 +36,7 @@ namespace FireRatingCloud
     public string computername { get; set; }
   }
 #endif // USE_JavaScriptSerializer
+  #endregion // Project data helper class for JavaScriptSerializer
 
   #region Cmd_1_CreateAndBindSharedParameter
   /// <summary>
@@ -198,240 +200,6 @@ namespace FireRatingCloud
   public class Cmd_2_ExportSharedParameterValues : IExternalCommand
   {
     /// <summary>
-    /// GET, PUT or POST JSON document data from or to 
-    /// the specified mongoDB collection.
-    /// </summary>
-    string QueryOrUpsert(
-      string collection_name_id_query,
-      string json,
-      string method )
-    {
-      string uri = Util.RestApiUri + "/" + collection_name_id_query;
-
-      HttpWebRequest request = HttpWebRequest.Create(
-        uri ) as HttpWebRequest;
-
-      request.ContentType = "application/json; charset=utf-8";
-      request.Accept = "application/json, text/javascript, */*";
-      request.Timeout = Util.Timeout;
-      request.Method = method;
-
-      if( 0 < json.Length )
-      {
-        Debug.Assert( !method.Equals( "GET" ),
-          "content is not allowed with GET" );
-
-        using( StreamWriter writer = new StreamWriter(
-          request.GetRequestStream() ) )
-        {
-          writer.Write( json );
-        }
-      }
-      WebResponse response = request.GetResponse();
-      Stream stream = response.GetResponseStream();
-      string jsonResponse = string.Empty;
-
-      using( StreamReader reader = new StreamReader(
-        stream ) )
-      {
-        while( !reader.EndOfStream )
-        {
-          jsonResponse += reader.ReadLine();
-        }
-      }
-      return jsonResponse;
-    }
-
-#if LOTS_OF_TEST_CODE
-    /// <summary>
-    /// POST JSON data to the specified mongoDB collection.
-    /// </summary>
-    async void PostJsonDataAsyncAttempt(
-      string collection_name,
-      string json )
-    {
-      using( System.Net.Http.HttpClient httpClient
-        = new System.Net.Http.HttpClient() )
-      {
-        try
-        {
-          string resourceAddress = Util.RestApiUri
-            + "/" + collection_name;
-
-          string postBody = json;
-
-          httpClient.DefaultRequestHeaders.Accept.Add(
-            new MediaTypeWithQualityHeaderValue(
-              "application/json" ) );
-
-          HttpResponseMessage wcfResponse
-            = await httpClient.PostAsync(
-              resourceAddress, new StringContent(
-                postBody, Encoding.UTF8,
-                "application/json" ) );
-
-          //await DisplayTextResult( wcfResponse, OutputField );
-        }
-        catch( HttpRequestException hre )
-        {
-          Debug.Print( "Error:" + hre.Message );
-        }
-        catch( TaskCanceledException )
-        {
-          Debug.Print( "Request canceled." );
-        }
-        catch( Exception ex )
-        {
-          Debug.Print( ex.Message );
-        }
-      }
-    }
-
-    static async void DownloadPageAsync()
-    {
-      // ... Target page.
-      string page = "http://en.wikipedia.org/";
-
-      // ... Use HttpClient.
-      using( HttpClient client = new HttpClient() )
-      using( HttpResponseMessage response = await client.GetAsync( page ) )
-      using( HttpContent content = response.Content )
-      {
-        // ... Read the string.
-        string result = await content.ReadAsStringAsync();
-
-        // ... Display the result.
-        if( result != null &&
-        result.Length >= 50 )
-        {
-          Console.WriteLine( result.Substring( 0, 50 ) + "..." );
-        }
-      }
-    }
-
-#if FUTURE_SAMPLE_CODE
-    static void DownloadPageAsync2( string[] args )
-    {
-      string uri = Util.RestApiUri;
-
-      // Create an HttpClient instance 
-      HttpClient client = new HttpClient();
-
-      // Send a request asynchronously continue when complete 
-      client.GetAsync( uri ).ContinueWith(
-          ( requestTask ) =>
-          {
-            // Get HTTP response from completed task. 
-            HttpResponseMessage response = requestTask.Result;
-
-            // Check that response was successful or throw exception 
-            response.EnsureSuccessStatusCode();
-
-            // Read response asynchronously as JsonValue and write out top facts for each country 
-            response.Content.ReadAsAsync<JsonArray>().ContinueWith(
-                ( readTask ) =>
-                {
-                  Console.WriteLine( "First 50 countries listed by The World Bank..." );
-                  foreach( var country in readTask.Result[1] )
-                  {
-                    Console.WriteLine( "   {0}, Country Code: {1}, Capital: {2}, Latitude: {3}, Longitude: {4}",
-                        country.Value["name"],
-                        country.Value["iso2Code"],
-                        country.Value["capitalCity"],
-                        country.Value["latitude"],
-                        country.Value["longitude"] );
-                  }
-                } );
-          } );
-
-      Console.WriteLine( "Hit ENTER to exit..." );
-      Console.ReadLine();
-    }
-#endif // FUTURE_SAMPLE_CODE
-
-#if USE_CODE_FROM_TWGL_EXPORT
-    /// <summary>
-    /// HTTP POST with the given JSON data in the 
-    /// request body. Use a local or global base URL.
-    /// </summary>
-    static public bool PostJsonData( string json )
-    {
-      bool rc = false;
-
-      string uri = Util.RestApiUri + "/" + projects;
-
-      HttpWebRequest req = WebRequest.Create( uri ) as HttpWebRequest;
-
-      req.KeepAlive = false;
-      req.Method = WebRequestMethods.Http.Post;
-
-      // Turn our request string into a byte stream.
-
-      byte[] postBytes = Encoding.UTF8.GetBytes( json );
-
-      req.ContentLength = postBytes.Length;
-
-      // Specify content type.
-
-      req.ContentType = "application/json; charset=UTF-8"; // or just "text/json"?
-      req.Accept = "application/json";
-      req.ContentLength = postBytes.Length;
-
-      Stream requestStream = req.GetRequestStream();
-      requestStream.Write( postBytes, 0, postBytes.Length );
-      requestStream.Close();
-
-      HttpWebResponse res = req.GetResponse() as HttpWebResponse;
-
-      string result;
-
-      using( StreamReader reader = new StreamReader(
-        res.GetResponseStream() ) )
-      {
-        result = reader.ReadToEnd();
-      }
-
-      // Get JavaScript modules from server public folder.
-
-      result = result.Replace( "<script src=\"/",
-        "<script src=\"" + base_url + "/" );
-
-      string filename = Path.GetTempFileName();
-      filename = Path.ChangeExtension( filename, "html" );
-
-      //string dir = Path.GetDirectoryName( filename );
-
-      //// Get JavaScript modules from current directory.
-
-      //string path = dir
-      //  .Replace( Path.GetPathRoot( dir ), "" )
-      //  .Replace( '\\', '/' );
-
-      ////result = result.Replace( "<script src=\"/",
-      ////  "<script src=\"file:///" + dir + "/" ); // XMLHttpRequest cannot load file:///C:/Users/tammikj/AppData/Local/Temp/vs.js. Cross origin requests are only supported for protocol schemes: http, data, chrome, chrome-extension, https, chrome-extension-resource.
-
-      //result = result.Replace( "<script src=\"/", 
-      //  "<script src=\"" );
-
-      //if( EnsureJsModulesPresent( dir ) )
-
-      {
-        using( StreamWriter writer = File.CreateText( filename ) )
-        {
-          writer.Write( result );
-          writer.Close();
-        }
-
-        System.Diagnostics.Process.Start( filename );
-
-        rc = true;
-      }
-      return rc;
-    }
-#endif // USE_CODE_FROM_TWGL_EXPORT
-#endif // LOTS_OF_TEST_CODE
-
-    /// <summary>
     /// Retrieve the project identification information 
     /// to store in the external database and return it
     /// as a dictionary in a JSON formatted string.
@@ -468,6 +236,7 @@ namespace FireRatingCloud
 
       return "{" + s + "}";
 
+      #region Use JavaScriptSerializer
 #if USE_JavaScriptSerializer
       // Use JavaScriptSerializer to format JSON data.
 
@@ -485,6 +254,7 @@ namespace FireRatingCloud
       return new JavaScriptSerializer().Serialize(
         project_data );
 #endif // USE_JavaScriptSerializer
+      #endregion // Use JavaScriptSerializer
     }
 
     /// <summary>
@@ -558,7 +328,7 @@ namespace FireRatingCloud
       string query = "projects/uid/"
         + doc.ProjectInformation.UniqueId;
 
-      string jsonResponse = QueryOrUpsert( query,
+      string jsonResponse = Util.QueryOrUpsert( query,
         string.Empty, "GET" );
 
       if( 0 < jsonResponse.Length )
@@ -569,12 +339,12 @@ namespace FireRatingCloud
         d = obj as Hashtable;
         project_id = d["_id"] as string;
 
-        jsonResponse = QueryOrUpsert(
+        jsonResponse = Util.QueryOrUpsert(
           "projects/" + project_id, json, "PUT" );
       }
       else
       {
-        jsonResponse = QueryOrUpsert(
+        jsonResponse = Util.QueryOrUpsert(
           "projects", json, "POST" );
       }
 
@@ -613,7 +383,7 @@ namespace FireRatingCloud
 
           Debug.Print( json );
 
-          jsonResponse = QueryOrUpsert(
+          jsonResponse = Util.QueryOrUpsert(
             "doors/" + e.UniqueId, json, "PUT" );
 
           Debug.Print( jsonResponse );
--- FireRatingCloud/Util.cs
+++ FireRatingCloud/Util.cs
@@ -3,8 +3,10 @@ using Autodesk.Revit.ApplicationServices;
 using Autodesk.Revit.DB;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 #endregion // Namespaces
@@ -44,6 +46,243 @@ namespace FireRatingCloud
         return base_url + "/" + _api_version;
       }
     }
+
+    /// <summary>
+    /// GET, PUT or POST JSON document data from or to 
+    /// the specified mongoDB collection.
+    /// </summary>
+    public static string QueryOrUpsert(
+      string collection_name_id_query,
+      string json,
+      string method )
+    {
+      string uri = Util.RestApiUri + "/" + collection_name_id_query;
+
+      HttpWebRequest request = HttpWebRequest.Create(
+        uri ) as HttpWebRequest;
+
+      request.ContentType = "application/json; charset=utf-8";
+      request.Accept = "application/json, text/javascript, */*";
+      request.Timeout = Util.Timeout;
+      request.Method = method;
+
+      if( 0 < json.Length )
+      {
+        Debug.Assert( !method.Equals( "GET" ),
+          "content is not allowed with GET" );
+
+        using( StreamWriter writer = new StreamWriter(
+          request.GetRequestStream() ) )
+        {
+          writer.Write( json );
+        }
+      }
+      WebResponse response = request.GetResponse();
+      Stream stream = response.GetResponseStream();
+      string jsonResponse = string.Empty;
+
+      using( StreamReader reader = new StreamReader(
+        stream ) )
+      {
+        while( !reader.EndOfStream )
+        {
+          jsonResponse += reader.ReadLine();
+        }
+      }
+      return jsonResponse;
+    }
+
+    #region Test Code
+#if LOTS_OF_TEST_CODE
+    /// <summary>
+    /// POST JSON data to the specified mongoDB collection.
+    /// </summary>
+    async void PostJsonDataAsyncAttempt(
+      string collection_name,
+      string json )
+    {
+      using( System.Net.Http.HttpClient httpClient
+        = new System.Net.Http.HttpClient() )
+      {
+        try
+        {
+          string resourceAddress = Util.RestApiUri
+            + "/" + collection_name;
+
+          string postBody = json;
+
+          httpClient.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue(
+              "application/json" ) );
+
+          HttpResponseMessage wcfResponse
+            = await httpClient.PostAsync(
+              resourceAddress, new StringContent(
+                postBody, Encoding.UTF8,
+                "application/json" ) );
+
+          //await DisplayTextResult( wcfResponse, OutputField );
+        }
+        catch( HttpRequestException hre )
+        {
+          Debug.Print( "Error:" + hre.Message );
+        }
+        catch( TaskCanceledException )
+        {
+          Debug.Print( "Request canceled." );
+        }
+        catch( Exception ex )
+        {
+          Debug.Print( ex.Message );
+        }
+      }
+    }
+
+    static async void DownloadPageAsync()
+    {
+      // ... Target page.
+      string page = "http://en.wikipedia.org/";
+
+      // ... Use HttpClient.
+      using( HttpClient client = new HttpClient() )
+      using( HttpResponseMessage response = await client.GetAsync( page ) )
+      using( HttpContent content = response.Content )
+      {
+        // ... Read the string.
+        string result = await content.ReadAsStringAsync();
+
+        // ... Display the result.
+        if( result != null &&
+        result.Length >= 50 )
+        {
+          Console.WriteLine( result.Substring( 0, 50 ) + "..." );
+        }
+      }
+    }
+
+#if FUTURE_SAMPLE_CODE
+    static void DownloadPageAsync2( string[] args )
+    {
+      string uri = Util.RestApiUri;
+
+      // Create an HttpClient instance 
+      HttpClient client = new HttpClient();
+
+      // Send a request asynchronously continue when complete 
+      client.GetAsync( uri ).ContinueWith(
+          ( requestTask ) =>
+          {
+            // Get HTTP response from completed task. 
+            HttpResponseMessage response = requestTask.Result;
+
+            // Check that response was successful or throw exception 
+            response.EnsureSuccessStatusCode();
+
+            // Read response asynchronously as JsonValue and write out top facts for each country 
+            response.Content.ReadAsAsync<JsonArray>().ContinueWith(
+                ( readTask ) =>
+                {
+                  Console.WriteLine( "First 50 countries listed by The World Bank..." );
+                  foreach( var country in readTask.Result[1] )
+                  {
+                    Console.WriteLine( "   {0}, Country Code: {1}, Capital: {2}, Latitude: {3}, Longitude: {4}",
+                        country.Value["name"],
+                        country.Value["iso2Code"],
+                        country.Value["capitalCity"],
+                        country.Value["latitude"],
+                        country.Value["longitude"] );
+                  }
+                } );
+          } );
+
+      Console.WriteLine( "Hit ENTER to exit..." );
+      Console.ReadLine();
+    }
+#endif // FUTURE_SAMPLE_CODE
+
+#if USE_CODE_FROM_TWGL_EXPORT
+    /// <summary>
+    /// HTTP POST with the given JSON data in the 
+    /// request body. Use a local or global base URL.
+    /// </summary>
+    static public bool PostJsonData( string json )
+    {
+      bool rc = false;
+
+      string uri = Util.RestApiUri + "/" + projects;
+
+      HttpWebRequest req = WebRequest.Create( uri ) as HttpWebRequest;
+
+      req.KeepAlive = false;
+      req.Method = WebRequestMethods.Http.Post;
+
+      // Turn our request string into a byte stream.
+
+      byte[] postBytes = Encoding.UTF8.GetBytes( json );
+
+      req.ContentLength = postBytes.Length;
+
+      // Specify content type.
+
+      req.ContentType = "application/json; charset=UTF-8"; // or just "text/json"?
+      req.Accept = "application/json";
+      req.ContentLength = postBytes.Length;
+
+      Stream requestStream = req.GetRequestStream();
+      requestStream.Write( postBytes, 0, postBytes.Length );
+      requestStream.Close();
+
+      HttpWebResponse res = req.GetResponse() as HttpWebResponse;
+
+      string result;
+
+      using( StreamReader reader = new StreamReader(
+        res.GetResponseStream() ) )
+      {
+        result = reader.ReadToEnd();
+      }
+
+      // Get JavaScript modules from server public folder.
+
+      result = result.Replace( "<script src=\"/",
+        "<script src=\"" + base_url + "/" );
+
+      string filename = Path.GetTempFileName();
+      filename = Path.ChangeExtension( filename, "html" );
+
+      //string dir = Path.GetDirectoryName( filename );
+
+      //// Get JavaScript modules from current directory.
+
+      //string path = dir
+      //  .Replace( Path.GetPathRoot( dir ), "" )
+      //  .Replace( '\\', '/' );
+
+      ////result = result.Replace( "<script src=\"/",
+      ////  "<script src=\"file:///" + dir + "/" ); // XMLHttpRequest cannot load file:///C:/Users/tammikj/AppData/Local/Temp/vs.js. Cross origin requests are only supported for protocol schemes: http, data, chrome, chrome-extension, https, chrome-extension-resource.
+
+      //result = result.Replace( "<script src=\"/", 
+      //  "<script src=\"" );
+
+      //if( EnsureJsModulesPresent( dir ) )
+
+      {
+        using( StreamWriter writer = File.CreateText( filename ) )
+        {
+          writer.Write( result );
+          writer.Close();
+        }
+
+        System.Diagnostics.Process.Start( filename );
+
+        rc = true;
+      }
+      return rc;
+    }
+#endif // USE_CODE_FROM_TWGL_EXPORT
+#endif // LOTS_OF_TEST_CODE
+    #endregion // Test Code
+
     #endregion // HTTP Access
 
     #region Shared Parameter Definition
