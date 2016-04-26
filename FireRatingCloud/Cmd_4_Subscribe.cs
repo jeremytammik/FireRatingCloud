53a50c5c2a5e72941f6c650788ec217beaa87951
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2016-04-26 19:06:13 +0200

    polishing for publication

--- FireRatingCloud/Cmd_4_Subscribe.cs
+++ FireRatingCloud/Cmd_4_Subscribe.cs
@@ -6,29 +6,29 @@ using Autodesk.Revit.UI;
 
 namespace FireRatingCloud
 {
-[Transaction( TransactionMode.ReadOnly )]
-class Cmd_4_Subscribe : IExternalCommand
-{
+  [Transaction( TransactionMode.ReadOnly )]
+  class Cmd_4_Subscribe : IExternalCommand
+  {
     public Result Execute(
     ExternalCommandData commandData,
     ref string message,
     ElementSet elements )
-  {
-    UIApplication uiapp = commandData.Application;
-    Document doc = uiapp.ActiveUIDocument.Document;
-
-    // Determine custom project identifier.
-
-    string project_id = Util.GetProjectIdentifier( doc );
-
-    if ( !App.Subscribed && 0 == DbAccessor.Timestamp )
     {
-      DbAccessor.Init( project_id );
+      UIApplication uiapp = commandData.Application;
+      Document doc = uiapp.ActiveUIDocument.Document;
+
+      // Determine custom project identifier.
+
+      string project_id = Util.GetProjectIdentifier( doc );
+
+      if ( !App.Subscribed && 0 == DbAccessor.Timestamp )
+      {
+        DbAccessor.Init( project_id );
+      }
+
+      DbAccessor.ToggleSubscription( uiapp );
+
+      return Result.Succeeded;
     }
-
-    DbAccessor.ToggleSubscription( uiapp );
-
-    return Result.Succeeded;
   }
-}
 }
